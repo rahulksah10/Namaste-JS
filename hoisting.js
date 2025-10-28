7eaad47b247e871f927968b824c583c3b07d9380
Rahul kumar sah <rahulraj12777@gmail.com>
2025-10-28 23:27:52 +0530

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+getName()
+console.log(a);
+console.log(getName);
+
+let a =7
+function getName(){
+    console.log("Namaste Rahul");
+}
+
+// getName()
+// console.log(a);  if we execute this part only we will not gate any eror while excuting the same code in before initilizaing the function it will excuted properly but not for the variable , for variable it will shows error like  "Cannot access 'a' before initialization"  or  "undefined"
